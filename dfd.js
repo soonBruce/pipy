680ea8e81ae3a83cbac5b57e54b74f557d3e4184
soonBruce <55883569+soonBruce@users.noreply.github.com>
2020-02-27 11:15:00 -0500

    Add files via upload

---
+++ dfd.js
@@ -0,0 +1,6 @@
+﻿ window.open = function() {};
+            window.print = function() {};
+            // Support hover state for mobile.
+            if (false) {
+                window.ontouchstart = function() {};
+            }
